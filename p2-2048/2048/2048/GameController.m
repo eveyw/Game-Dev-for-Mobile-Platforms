d1ec40133c28d20bfd6f710b396ce1256793696e
Wen Yang <wyang41@binghamton.edu>
2019-02-10 23:53:36 -0500

    touch and control

--- p2-2048/2048/2048/GameController.m
+++ p2-2048/2048/2048/GameController.m
@@ -12,6 +12,53 @@
 
 @interface GameController ()
 
+// 触摸移动
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
+    if(!_startTime){
+        _startTime = NO;
+        [self startTimer];
+    }
+    [super touchesMoved:touches withEvent:event];
+    UITouch *touch = [touches anyObject];
+    
+    CGPoint previousLocation = [touch previousLocationInView:self.view];
+    CGPoint currentLocation = [touch locationInView:self.view];
+    
+    int x = currentLocation.x - previousLocation.x;
+    int y = currentLocation.y - previousLocation.y;
+    int ax = x>0?x:0-x;
+    int ay = y>0?y:0-y;
+    
+    //对开始点击屏幕的位置判断是否在游戏区域
+    if(currentLocation.y<(_gameY-_tiledmapheight/2) ||
+       currentLocation.y>(_gameY+_tiledmapheight/2) ||
+       currentLocation.x<(_gameX-_tiledmapwidth/2)||
+       currentLocation.x>(_gameX+_tiledmapwidth/2)){
+        _startMove = NO;
+    }
+    
+    //移动像素大于5  则滑动成功 判断方向
+    if(_startMove && ( ax>5 || ay>5)){
+        _startMove = NO;
+        if(ax>ay){
+            if(x>0){
+                _directionType = RIGHT;
+            }else{
+                _directionType = LEFT;
+            }
+        }else{
+            if(y<0){
+                _directionType = UP;
+            }else{
+                _directionType = DOWN;
+            }
+        }
+        [self moveAllTiled:_directionType];
+    }
+}
+
+
+
+
 @end
 
-@implementation GameController
--- p2-2048/2048/2048/MoveView.m
+++ p2-2048/2048/2048/MoveView.m
@@ -8,11 +8,87 @@
 
 
 #import "MoveView.h"
+@implementation MoveTieldView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self){
+        [self initMainView];
+    }
+    return self;
+}
 
 
+-(void)initMainView
+{
+    int n = arc4random() % 10;
+    _number = (n == 5)?4:2;
+    
+    
+    _label = [[UILabel alloc]init];
+    _label.frame = makeRect(0, 0, GAME_TILED_WIDTH, GAME_TILED_HEIGTH);
+    _label.tag = 10;
+    [_label setText:[NSString stringWithFormat:@"%d",_number]];
+    _label.font = [UIFont boldSystemFontOfSize:30];
+    _label.textColor = TILEDColor;
+    _label.textAlignment = NSTextAlignmentCenter;
+    [self addSubview:_label];
+    
+    self.backgroundColor = [UIColor setTiledColor:_number];
+    self.tag = 110;
+    
+    if(_number>=1000){
+        _label.font = [UIFont boldSystemFontOfSize:20];
+    }
+    
+}
 
-@implementation MoveView
 
+-(void)showAtx:(int)r
+       showAty:(int)w{
+    
+    _row = r;
+    _col = w;
+    
+    self.frame = makeRect(_col*GAME_TILED_WIDTH+GAME_TILED_BOARD_WIDTH*(_col+1),
+                          _row*GAME_TILED_HEIGTH+GAME_TILED_BOARD_WIDTH*(_row+1),
+                          GAME_TILED_WIDTH,GAME_TILED_HEIGTH);
+    [self changeBig];
+}
+
+
+-(void)moveTox:(int)r
+       moveToy:(int)c{
+    _row = r;
+    _col = c;
+    [UIView animateWithDuration:0.1f animations:^
+     {
+         self.frame = makeRect(_col*GAME_TILED_WIDTH+GAME_TILED_BOARD_WIDTH*(_col+1),
+                               _row*GAME_TILED_HEIGTH+GAME_TILED_BOARD_WIDTH*(_row+1),
+                               GAME_TILED_WIDTH,GAME_TILED_HEIGTH);
+     } completion:nil];
+}
+
+
+-(void)doubleNumber{
+    
+    _number *=2;
+    _label.text = [NSString stringWithFormat:@"%d",_number];
+    [self changeBig];
+    
+    int num=0;
+    int k=_number;
+    while(k!=1){
+        num++;
+        k=k/2;
+    }
+    self.backgroundColor = [UIColor setTiledColor:_number];
+    _label.textColor = _number<=4?TILEDColor:makeColorRgb(248, 245, 245);
+    if(num>=10){
+        _label.font = [UIFont boldSystemFontOfSize:20];
+    }
+}
 
 
 
