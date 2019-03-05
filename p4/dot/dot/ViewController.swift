c2a44ee19e5390a78cc97c070a575cfa51ab567b
Wen Yang <wyang41@binghamton.edu>
2019-03-05 15:26:22 -0500

    u

--- p4/dot/dot/ViewController.swift
+++ p4/dot/dot/ViewController.swift
@@ -24,6 +24,26 @@ class ViewController: UIViewController {
         case gameOver
     }
     
+    // MARK: - Constants
+    fileprivate let radius: CGFloat = 10
+    fileprivate let playerAnimationDuration = 5.0
+    fileprivate let enemySpeed: CGFloat = 60 // points per second
+    fileprivate let colors = [#colorLiteral(red: 0.08235294118, green: 0.6980392157, blue: 0.5411764706, alpha: 1), #colorLiteral(red: 0.07058823529, green: 0.5725490196, blue: 0.4470588235, alpha: 1), #colorLiteral(red: 0.9333333333, green: 0.7333333333, blue: 0, alpha: 1), #colorLiteral(red: 0.9411764706, green: 0.5450980392, blue: 0, alpha: 1), #colorLiteral(red: 0.1411764706, green: 0.7803921569, blue: 0.3529411765, alpha: 1), #colorLiteral(red: 0.1176470588, green: 0.6431372549, blue: 0.2941176471, alpha: 1), #colorLiteral(red: 0.8784313725, green: 0.4156862745, blue: 0.03921568627, alpha: 1), #colorLiteral(red: 0.7882352941, green: 0.2470588235, blue: 0, alpha: 1), #colorLiteral(red: 0.1490196078, green: 0.5098039216, blue: 0.8352941176, alpha: 1), #colorLiteral(red: 0.1137254902, green: 0.4156862745, blue: 0.6784313725, alpha: 1), #colorLiteral(red: 0.8823529412, green: 0.2, blue: 0.1607843137, alpha: 1), #colorLiteral(red: 0.7019607843, green: 0.1411764706, blue: 0.1098039216, alpha: 1), #colorLiteral(red: 0.537254902, green: 0.2352941176, blue: 0.662745098, alpha: 1), #colorLiteral(red: 0.4823529412, green: 0.1490196078, blue: 0.6235294118, alpha: 1), #colorLiteral(red: 0.6862745098, green: 0.7137254902, blue: 0.7333333333, alpha: 1), #colorLiteral(red: 0.1529411765, green: 0.2196078431, blue: 0.2980392157, alpha: 1), #colorLiteral(red: 0.1294117647, green: 0.1843137255, blue: 0.2470588235, alpha: 1), #colorLiteral(red: 0.5137254902, green: 0.5843137255, blue: 0.5843137255, alpha: 1), #colorLiteral(red: 0.4235294118, green: 0.4745098039, blue: 0.4784313725, alpha: 1)]
+    
+    // MARK: - fileprivate
+    fileprivate var playerView = UIView(frame: .zero)
+    fileprivate var playerAnimator: UIViewPropertyAnimator?
+    
+    fileprivate var enemyViews = [UIView]()
+    fileprivate var enemyAnimators = [UIViewPropertyAnimator]()
+    fileprivate var enemyTimer: Timer?
+    
+    fileprivate var displayLink: CADisplayLink?
+    fileprivate var beginTimestamp: TimeInterval = 0
+    fileprivate var elapsedTime: TimeInterval = 0
+    
+    fileprivate var gameState = GameState.ready
+    
     // 53
     override func viewDidLoad() {
         super.viewDidLoad()
