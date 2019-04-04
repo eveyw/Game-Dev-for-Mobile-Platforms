924cae6cbb2d7276d05d65a414d4b82875f37827
Wen Yang <wyang41@binghamton.edu>
2019-04-04 18:04:36 -0400

    module

--- p5-team/p5-team/caculator.swift
+++ p5-team/p5-team/caculator.swift
@@ -10,5 +10,35 @@ import Foundation
 
 class calculator
 {
+    private var accumulator = 0.0
+    private var internalProgram = [AnyObject]()
+
+    func setOperand(operand: Double) {
+        accumulator = operand
+        internalProgram.append(operand as AnyObject)
+    }
+    
+    private var operations: Dictionary<String, Operation> = [
+        "π": Operation.Constant(Double.pi),
+        "e": Operation.Constant(M_E),
+        "±": Operation.UnaryOperation({ -$0 }),
+        "√": Operation.UnaryOperation(sqrt),
+        "cos": Operation.UnaryOperation(cos),
+        "×": Operation.BinaryOperation({ $0 * $1 }),
+        "÷": Operation.BinaryOperation({ $0 / $1 }),
+        "+": Operation.BinaryOperation({ $0 + $1 }),
+        "−": Operation.BinaryOperation({ $0 - $1 }),
+        "=": Operation.Equals
+    ]
+    
+    private enum operation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
+    }
+    
+        func performoperation(symbol: String) {
+    }
     
 }
