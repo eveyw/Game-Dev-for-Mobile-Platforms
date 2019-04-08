68e978575d6d2631bc4b608a3dd3fc7459fcc520
Wen Yang <wyang41@binghamton.edu>
2019-04-08 12:10:35 -0400

    p6

---
+++ p6-WebBackEnd/p6-WebBackEndTests/p6_WebBackEndTests.swift
@@ -0,0 +1,34 @@
+//
+//  p6_WebBackEndTests.swift
+//  p6-WebBackEndTests
+//
+//  Created by WEN YANG on 4/8/19.
+//  Copyright © 2019 WEN YANG. All rights reserved.
+//
+
+import XCTest
+@testable import p6_WebBackEnd
+
+class p6_WebBackEndTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
